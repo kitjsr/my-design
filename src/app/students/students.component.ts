1f07cd1ffad002b621fbc652aeaa4049dc9a2616
kitjsr <91665868+kitjsr@users.noreply.github.com>
2023-03-02 20:42:16 +0530

    changes

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -26,4 +26,30 @@ export class StudentsComponent {
   students: Student[] = studentsData;
   employees: Employee[] = employeesData;
   mydatas: any = myData;
+  gid=0;
+
+  gallery = [
+    {photo: 'https://jsonformatter.org/img/Robert-Downey-Jr.jpg', gid: 1}, 
+    {photo: 'https://jsonformatter.org/img/Maria-Sharapova.jpg', gid: 2}, 
+    {photo: 'https://jsonformatter.org/img/Robert-Downey-Jr.jpg', gid: 1}, 
+    {photo: 'https://jsonformatter.org/img/Maria-Sharapova.jpg', gid: 2},
+    {photo: 'https://jsonformatter.org/img/tom-cruise.jpg', gid: 3}
+  ];
+
+  constructor() { }
+
+  isGalleryType(g: any): boolean {
+    if(g.gid === this.gid){
+      return true;
+    }
+    else if(this.gid === 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  fun(gid:any){
+    this.gid=gid;
+  }
 }
